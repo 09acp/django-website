e0f7dc74d39451984f0d238246dc2ef4d7d58d78
Alex Papakyriacou <09alexpapa@google.com>
2023-09-17 12:26:17 +0300

    wolfx.io fix for static -Test.

--- tg_site/settings.py
+++ tg_site/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,8 +24,8 @@ SECRET_KEY = 'django-insecure-1u09#34(3zjwt71q(e915$*rol1tf6p&kmz-%i%a5cop^yv0+_
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
+# AWS LIGHTSAIL STATIC IP (PUBLIC)
+ALLOWED_HOSTS = ['18.193.92.213']
 
 
 # Application definition
@@ -115,8 +115,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+# STATIC_URL = 'static/'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'website/static'),
+]
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_URL = 'website/templates/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
